e67d2bb9a19b9bf7ec116089d228a4402f120efc
Maxime François <mfrancois@big-boss-studio.com>
2016-08-03 17:47:53 +0200

    Fix preg_match_fallback

--- src/Distilleries/Expendable/Helpers/Security.php
+++ src/Distilleries/Expendable/Helpers/Security.php
@@ -350,9 +350,12 @@ class Security
         }
 
         $str = html_entity_decode($str, ENT_COMPAT, $charset);
-        $str = preg_replace('~&#x(0*[0-9a-f]{2,5})~ei', 'chr(hexdec("\\1"))', $str);
-
-        return preg_replace('~&#([0-9]{2,4})~e', 'chr(\\1)', $str);
+        $str = preg_replace_callback('~&#x(0*[0-9a-f]{2,5})~i', function($matches) {
+            return chr(hexdec($matches[1]));
+        }, $str);
+        return preg_replace_callback('~&#([0-9]{2,4})~', function($matches) {
+            return chr($matches[1]);
+        }, $str);
     }
 
     // --------------------------------------------------------------------
