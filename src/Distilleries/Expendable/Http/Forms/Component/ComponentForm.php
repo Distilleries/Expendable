ae1877518ed30e5c89452c42f121ce12ae727f52
mfrancois <mfrancois@big-boss-studio.com>
2018-04-07 23:17:12 +0200

    Add check if directory exist

--- src/Distilleries/Expendable/Http/Forms/Component/ComponentForm.php
+++ src/Distilleries/Expendable/Http/Forms/Component/ComponentForm.php
@@ -3,10 +3,11 @@
 use Distilleries\Expendable\Helpers\StaticLabel;
 use \File;
 
-class ComponentForm extends \Distilleries\FormBuilder\FormValidator {
+class ComponentForm extends \Distilleries\FormBuilder\FormValidator
+{
 
     public static $rules = [
-        'libelle'   => 'required'
+        'libelle' => 'required'
     ];
     public static $rules_update = null;
 
@@ -16,28 +17,28 @@ class ComponentForm extends \Distilleries\FormBuilder\FormValidator {
         $this
             ->add('libelle', 'text', [
                 'validation' => 'required',
-                'label'      => trans('expendable::form.name'),
-                'help'       => trans('expendable::form.auto_sufix')
+                'label' => trans('expendable::form.name'),
+                'help' => trans('expendable::form.auto_sufix')
             ])
             ->add('state', 'choice', [
-                'choices'     => StaticLabel::states(),
+                'choices' => StaticLabel::states(),
                 'empty_value' => '-',
-                'label'       => trans('expendable::form.state'),
-                'expanded'    => true,
-                'multiple'    => true
+                'label' => trans('expendable::form.state'),
+                'expanded' => true,
+                'multiple' => true
             ])
             ->add('models', 'choice', [
-                'choices'     => $this->getChoiceModels(),
+                'choices' => $this->getChoiceModels(),
                 'empty_value' => '-',
-                'label'       => trans('expendable::form.model')
+                'label' => trans('expendable::form.model')
             ])
             ->add('colon_datatable', 'tag', [
                 'label' => trans('expendable::form.columns'),
-                'help'  => trans('expendable::form.help_colon_datatable')
+                'help' => trans('expendable::form.help_colon_datatable')
             ])
             ->add('fields_form', 'tag', [
                 'label' => trans('expendable::form.fields'),
-                'help'  => trans('expendable::form.help_fields_form')
+                'help' => trans('expendable::form.help_fields_form')
             ])
             ->addDefaultActions();
     }
@@ -46,29 +47,30 @@ class ComponentForm extends \Distilleries\FormBuilder\FormValidator {
     {
         $allModels = [
             [
-                'path'      => app_path() . DIRECTORY_SEPARATOR,
+                'path' => app_path() . DIRECTORY_SEPARATOR,
                 'namespace' => '{{app}}',
             ],
             [
-                'path'      => app_path() . DIRECTORY_SEPARATOR.'Models' . DIRECTORY_SEPARATOR,
+                'path' => app_path() . DIRECTORY_SEPARATOR . 'Models' . DIRECTORY_SEPARATOR,
                 'namespace' => '{{app}}Models\\',
             ],
             [
-                'path'      => __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Models' . DIRECTORY_SEPARATOR,
+                'path' => __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Models' . DIRECTORY_SEPARATOR,
                 'namespace' => 'Distilleries\\Expendable\\Models\\',
             ],
         ];
 
         $choices = [];
-        foreach ($allModels as $config)
-        {
-            $models = app('files')->files($config['path']);
-            foreach ($models as $model)
-            {
-                $choice                               = explode('/', $model);
-                $model                                = preg_replace('/.php/i', '', last($choice));
-                $choices[$config['namespace'] . $model] = $model;
+        foreach ($allModels as $config) {
+            if (app('files')->isDirectory($config['path'])) {
+                $models = app('files')->files($config['path']);
+                foreach ($models as $model) {
+                    $choice = explode('/', $model);
+                    $model = preg_replace('/.php/i', '', last($choice));
+                    $choices[$config['namespace'] . $model] = $model;
+                }
             }
+
         }
 
         return $choices;
